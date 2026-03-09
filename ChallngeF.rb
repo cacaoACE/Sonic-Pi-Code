0f187ad15ffddac1002e6feefaca2e00126d08fa
cacaoACE <maxfishmartinez@gmail.com>
2026-03-09 10:16:05 -0700

    Create ChallngeF.rb

---
+++ ChallngeF.rb
@@ -0,0 +1,21 @@
+#CHALLENGE F: Piano Glissando starter code
+use_synth :piano
+num = 21
+
+88.times do
+  play(num)
+  sleep 0.5
+  num = num + 1
+end
+
+88.times do
+  play(num)
+  sleep 0.25
+  num = num - 1
+end
+
+88.times do
+  play(num)
+  sleep 0.125
+  num = num + 1
+end
